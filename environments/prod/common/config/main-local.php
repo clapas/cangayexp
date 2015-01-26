3fa13a242a656140ce6f7f8ac54dfe02e6d49f2e
Claudio Pascual <claudio.pascual@gmail.com>
2015-01-26 11:57:16 +0000

    Missed prod environment configuration

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'pgsql:dbname=dc1u2612mb6v6r;host=ec2-54-221-249-3.compute-1.amazonaws.com;port=5432',
+            'username' => 'kvkbxoulmfoitl',
+            'password' => 'Ie8b4KURoF5wurHEjfmJGgzm-D',
             'charset' => 'utf8',
         ],
         'mailer' => [
