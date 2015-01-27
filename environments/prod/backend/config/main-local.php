fe2fbb8952410ac9a4de6c494764a1c8acaabc0a
Claudio Pascual <claudio.pascual@gmail.com>
2015-01-27 11:33:24 +0000

    Fixing still wrong configuration

--- environments/prod/backend/config/main-local.php
+++ environments/prod/backend/config/main-local.php
@@ -6,6 +6,9 @@ return [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
+        'urlManager' => [
+            'baseUrl' => '/admin'
+        ],
         'session' => [
             'name' => 'PHPFRONTSESSID',
             'savePath' => __DIR__ . '/../tmp',
--- environments/prod/frontend/config/main-local.php
+++ environments/prod/frontend/config/main-local.php
@@ -4,6 +4,10 @@ return [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
+            'baseUrl' => '/',
+        ],
+        'urlManager' => [
+            'baseUrl' => '/'
         ],
         'session' => [
             'name' => 'PHPFRONTSESSID',
