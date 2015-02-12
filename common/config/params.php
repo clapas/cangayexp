f94bceddd0745eb34fa0f4332b1eb9a56e9b81b5
Claudio Pascual <claudio.pascual@gmail.com>
2015-02-12 10:14:14 +0000

    Changed smartfile shared link base url

--- common/config/params.php
+++ common/config/params.php
@@ -5,6 +5,6 @@ return [
     'user.passwordResetTokenExpire' => 3600,
     'SF_API_KEY' => 'E8KXj4recJTrSErGys4Didiwc72Ab3',
     'SF_API_PWD' => 'seoOxXuK0W77I8czzz73EeaZxd0vvH',
-    'SF_base_url' => 'https://file.ac/ueuKI8ozkB4/',
+    'SF_base_url' => 'https://file.ac/K2rH150VF4k/',
     'SF_data_path' => '/path/data/lits/'
 ];
