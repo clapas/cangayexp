d2253224af2b9807ddfa98af282a7bd0cf96ce06
Claudio Pascual <claudio.pascual@gmail.com>
2017-01-03 14:54:34 +0000

    Entorno de producción en vultr

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:dbname=dc1u2612mb6v6r;host=ec2-54-221-249-3.compute-1.amazonaws.com;port=5432',
-            'username' => 'kvkbxoulmfoitl',
-            'password' => 'Ie8b4KURoF5wurHEjfmJGgzm-D',
+            'dsn' => 'pgsql:host=localhost;dbname=cangayexp',
+            'username' => 'cangayexp',
+            'password' => '`RR9d2L~X8y.tzmG',
             'charset' => 'utf8',
         ],
         'mailer' => [
